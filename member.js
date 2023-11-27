e4e2def11704963224ec04ed7d2e88db9304eb3a
TheLaucha <47804695+TheLaucha@users.noreply.github.com>
2023-11-27 14:04:09 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,25 @@
+function skillsMember() {
+    var skills = document.getElementById("skills");
+    var member = document.getElementById("member");
+    var memberName = document.getElementById("memberName");
+    var memberSkills = document.getElementById("memberSkills");
+    var memberSkillsList = document.getElementById("memberSkillsList");
+    var memberSkillsListItems = document.getElementsByClassName("memberSkillsListItem");
+    var memberSkillsListItem = document.getElementById("memberSkillsListItem");
+    var memberSkillsListItemText = document.getElementById("memberSkillsListItemText");
+    var memberSkillsListItemClose = document.getElementById("memberSkillsListItemClose");
+    var memberSkillsListItemCloseIcon = document.getElementById("memberSkillsListItemCloseIcon");
+    var memberSkillsListItemCloseIconPath = document.getElementById("memberSkillsListItemCloseIconPath");
+    var memberSkillsListItemCloseIconPathD = document.getElementById("memberSkillsListItemCloseIconPathD");
+    var memberSkillsListItemCloseIconPathDHover = document.getElementById("memberSkillsListItemCloseIconPathDHover");
+    var memberSkillsListItemCloseIconPathDHoverPath = document.getElementById("memberSkillsListItemCloseIconPathDHoverPath");
+    var memberSkillsListItemCloseIconPathDHoverPathD = document.getElementById("memberSkillsListItemCloseIconPathDHoverPathD");
+    var memberSkillsListItemCloseIconPathDHoverPathDHover = document.getElementById("memberSkillsListItemCloseIconPathDHoverPathDHover");
+    var memberSkillsListItemCloseIconPathDHoverPathDHoverPath = document.getElementById("memberSkillsListItemCloseIconPathDHoverPathDHoverPath");
+    var memberSkillsListItemCloseIconPathDHoverPathDHoverPathD = document.getElementById("memberSkillsListItemCloseIconPathDHoverPathDHoverPathD");
+    var memberSkillsListItemCloseIconPathDHoverPathDHoverPathDHover = document.getElementById("memberSkillsListItemCloseIconPathDHoverPathDHoverPathDHover");
+    var memberSkillsListItemCloseIconPathDHoverPathDHoverPathDHoverPath = document.getElementById("memberSkillsListItemCloseIconPathDHoverPathDHoverPathDHoverPath");
+    var memberSkillsListItemCloseIconPathDHoverPathDHoverPathDHoverPathD = document.getElementById("memberSkillsListItemCloseIconPathDHoverPathDHoverPathDHoverPathD");
+    var memberSkillsListItemCloseIconPathDHoverPathDHoverPathDHoverPathDHover = document.getElementById("memberSkillsListItemCloseIconPathDHoverPathDHoverPathDHoverPathDHover");
+    var memberSkillsListItemCloseIconPathDHoverPath
+}
